e0a1111e7576ba2672b1e2d5824489fae4ac9ac3
Crowdbotics <team@crowdbotics.com>
2022-02-03 19:31:27 +0000

    Update models

--- backend/aksam_crm_33339/wsgi.py
+++ backend/aksam_crm_33339/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aksam_crm_33339.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aksam_crm_33339.settings")
 
 application = get_wsgi_application()
